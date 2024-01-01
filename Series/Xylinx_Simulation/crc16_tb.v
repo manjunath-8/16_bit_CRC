79778ab984d25e69be3e5e82c592baa63dc054cd
Manjunath Inamati <manjunathinamati8@gmail.com>
2024-01-01 23:14:26 +0530

    Initial commit

--- Series/Xylinx_Simulation/crc16_tb.v
+++ Series/Xylinx_Simulation/crc16_tb.v
@@ -4,15 +4,15 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   09:24:35 12/16/2023
-// Design Name:   crc16
-// Module Name:   E:/MINI_PROJECT/Series_CRC/crc16_tb.v
-// Project Name:  Series_CRC
+// Create Date:   11:59:24 12/20/2023
+// Design Name:   try
+// Module Name:   E:/MINI_PROJECT/CRC_BLOCK/try_tb.v
+// Project Name:  CRC_BLOCK
 // Target Device:  
 // Tool versions:  
 // Description: 
 //
-// Verilog Test Fixture created by ISE for module: crc16
+// Verilog Test Fixture created by ISE for module: try
 //
 // Dependencies:
 // 
@@ -22,44 +22,33 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 
-module crc16_tb;
+module try_tb;
 
 	// Inputs
 	reg clk;
 	reg reset;
-	reg data_in;
-   integer i;
-	reg [33:0]data;
-	reg [15:0]crc;
-	
-	reg [49:0]in_put;
+	reg [33:0] data_in;
+
 	// Outputs
-	wire [15:0] crc_out;
+	wire [49:0] data_out;
 
 	// Instantiate the Unit Under Test (UUT)
-	crc16 uut (
+	try uut (
 		.clk(clk), 
 		.reset(reset), 
-		.data_in(data_in), 
-		.crc_out(crc_out)
+		.data_in(data_in),		
+		.data_out(data_out)
 	);
 
-initial begin
+	initial begin
         // Initialize signals
         clk = 1;
-		  data = 34'b1010101010101010101010101010101010;
-		  crc = 16'b0000000000000000;
-		  in_put ={data,crc};
-
+		  data_in = 34'b1010101010101010101010101010101010;
         // Apply reset
         reset = 1; #10;
 		  reset = 0;
-
-        for (i = 49; i >=0; i = i - 1)
-    		  begin
-             data_in = in_put[i]; #10;
-			  end
- end
+    end
     // Generate clock
-  always #5 clk = ~clk;
+	 always #5 clk = ~clk;
 endmodule
+
